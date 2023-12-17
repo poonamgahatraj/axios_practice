cf09c0f6a008f0e346d52c188e1e4dc2b316642c
Poonam Gahatraj <poonamgtrz@gmail.com>
2023-12-17 18:27:27 +0545

    axios_practice

---
+++ axios_practice/src/components/firstpage.vue
@@ -0,0 +1,20 @@
+<template>
+<h1>hello</h1>
+</template>
+
+<script scoped>
+import Vue from "vue";
+import axios from "axios";
+import VueAxios from "vue-axios";
+Vue.use(axios, VueAxios);
+
+export default {
+  mounted() {
+    Vue.axios
+      .get("https://api.coindesk.com/v1/bpi/currentprice.json")
+      .then((response) => {
+        console.log(response);
+      });
+  },
+};
+</script>
